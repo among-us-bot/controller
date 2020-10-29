56edc1cee94a2257763084f3c7fcc811252d6f76
TAG-Epic <admin@itzepic.net>
2020-10-29 01:13:52 +0100

    Feat: Add color logger

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ Created by Epic at 9/5/20
 from custom_types import ExtendedClient
 
 from os import environ as env
-from logging import basicConfig, DEBUG
+from logging import getLogger, WARNING
+from color_format import basicConfig
 
 client = ExtendedClient(default_prefix="/", intents=512)
-basicConfig(level=DEBUG)
-
+basicConfig(getLogger())
+getLogger("speedcord").setLevel(WARNING)
 client.cog_manager.register_cog("about")
 
 client.token = env["TOKEN"]
