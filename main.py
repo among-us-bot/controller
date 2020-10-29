996f8b1f3605f5c2e15fc5983e4f3b479a49e5b7
TAG-Epic <admin@itzepic.net>
2020-10-29 01:15:56 +0100

    Fix: intents

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from os import environ as env
 from logging import getLogger, WARNING
 from color_format import basicConfig
 
-client = ExtendedClient(default_prefix="/", intents=512)
+client = ExtendedClient(default_prefix="/", intents=641)
 basicConfig(getLogger())
 getLogger("speedcord").setLevel(WARNING)
 client.cog_manager.register_cog("about")
