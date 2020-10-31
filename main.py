1657b792eb1583fd90d7a04ff02a4dd7a2c6a1ad
TAG-Epic <admin@itzepic.net>
2020-10-31 22:45:42 +0100

    Feat: Automatically load all cogs from cog dir

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from custom_types import ExtendedClient
 from os import environ as env
 from logging import getLogger, WARNING, DEBUG
 from color_format import basicConfig
+from pathlib import Path
 
 client = ExtendedClient(default_prefix="/", intents=641)
 log = getLogger()
@@ -13,15 +14,10 @@ log.setLevel(DEBUG)
 basicConfig(log)
 #getLogger("speedcord").setLevel(WARNING)
 
-cogs = [
-    "about",
-    "debug",
-    "admin",
-    "config",
-    "scale_alert"
-]
+cog_path = Path("cogs/")
 
-for cog in cogs:
+for cog_path in cog_path.glob("*.py"):
+    cog = str(cog_path)[5:].split(".")[0]
     client.cog_manager.register_cog(cog)
 
 client.token = env["TOKEN"]
