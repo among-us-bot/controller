471dcdb085ee8f8a583120cf3fc3cbf1d20dddf8
TAG-Epic <admin@itzepic.net>
2020-11-05 23:25:58 +0100

    Feat: Make discord url a env var

--- cogs/about.py
+++ cogs/about.py
@@ -4,12 +4,14 @@ Created by Epic at 10/24/20
 from custom_types import CogType, ExtendedClient
 from cog_manager import CommandContext
 
+from os import environ as env
+
 
 class About(CogType):
     @CogType.command("about")
     async def about(self, ctx: CommandContext):
         r = await ctx.send("AQue is a bot to manage your among us matchmaking servers easily!\n"
-                           "Join https://discord.gg/tyqNFuU if you want it for yourself!")
+                           f"Join {env['discord']} if you want it for yourself!")
 
 
 def setup(bot: ExtendedClient):
