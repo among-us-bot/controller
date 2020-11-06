9f276bd7264d81293a89d665108baf6479e72341
TAG-Epic <admin@itzepic.net>
2020-11-06 19:40:46 +0100

    Fix: Rewrite help command to use new command internals

--- cogs/help.py
+++ cogs/help.py
@@ -4,13 +4,16 @@ Created by Epic at 11/6/20
 from custom_types import CogType, ExtendedClient
 from cog_manager import CommandContext
 
-from os import environ as env
-
 
 class Help(CogType):
-    @CogType.command("help")
-    async def about(self, ctx: CommandContext):
-        await ctx.send("\n".join([i[0] for i in self.bot.cog_manager.commands]))
+    @CogType.command("help ( ?\\w)*")
+    async def help(self, ctx: CommandContext):
+        command_name = ctx.args[0]
+        if command_name is None:
+            prefix = self.bot.get_prefix(ctx.message.guild_id)
+            header = f"__Use {prefix}help *command* to get info on a command__\n"
+            command_names = [command_details["name"] for command_details in self.bot.cog_manager.commands]
+            await ctx.send(header + "\n".join(command_names))
 
 
 def setup(bot: ExtendedClient):
